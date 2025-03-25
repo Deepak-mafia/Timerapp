365d58f52ace6c01d37a86b5aeb7fcdaf73f3a31
Deepak Chaudhary <cdeepak521@gmail.com>
2025-03-25 08:52:33 +0530

    removed unwanted code from starwars

--- App.js
+++ App.js
@@ -1,58 +1,11 @@
-import React, {useEffect, useState} from 'react';
-import {TimerProvider} from './TimerContext';
+import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-// import {MMKV, useMMKVBoolean} from 'react-native-mmkv';
-import {
-  LoggedInStack,
-  OnboardingStack,
-  TimerStack,
-  StarWarsStack,
-} from './src/Navigation/Stack';
-import {ThemeProvider} from './src/Theme/ThemeContext';
-import {ActivityIndicator, View} from 'react-native';
+import {StarWarsStack} from './src/Navigation/Stack';
 
-// export const storage = new MMKV();
 const App = () => {
-  // const [isLoggedIn, setIsLoggedIn] = useState(null);
-  // const [isLoading, setIsLoading] = useState(true);
-
-  // useEffect(() => {
-  //   const loadAuthState = async () => {
-  //     const hasIsLoggedIn = storage.contains('isLoggedIn');
-  //     if (hasIsLoggedIn) {
-  //       setIsLoggedIn(storage.getBoolean('isLoggedIn') || false);
-  //     } else {
-  //       setIsLoggedIn(false);
-  //       storage.set('isLoggedIn', false);
-  //     }
-  //     setIsLoading(false);
-  //   };
-
-  //   loadAuthState();
-  // }, []);
-
-  // if (isLoading) {
-  //   return (
-  //     <View
-  //       style={{
-  //         flex: 1,
-  //         justifyContent: 'center',
-  //         alignItems: 'center',
-  //         backgroundColor: '#242424', // Darker background for better contrast
-  //       }}>
-  //       <ActivityIndicator size="large" color="#4dabf7" />
-  //     </View>
-  //   );
-  // }
   return (
     <NavigationContainer>
-      {/* <ThemeProvider>
-      <TimerProvider> */}
       <StarWarsStack />
-      {/* {isLoggedIn ? <LoggedInStack /> : <OnboardingStack />} */}
-      {/* <TimerStack /> */}
-      {/* </TimerProvider>
-    </ThemeProvider> */}
     </NavigationContainer>
   );
 };
--- src/Navigation/Stack.js
+++ src/Navigation/Stack.js
@@ -1,8 +1,5 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import WelcomeScreen from '../Screens/Onboarding/WelcomeScreen';
-import HomeScreen from '../Screens/LoggedIn/HomeScreen';
-import {HistoryScreen} from '../Timercomp/HistoryScreen';
-import {HomeScreen as TimerHomeScreen} from '../Timercomp/HomeScreen';
+
 import Login from '../Screens/StarWars/Login';
 import Search from '../Screens/StarWars/Search';
 
@@ -21,67 +18,4 @@ const StarWarsStack = () => {
   );
 };
 
-// Onboarding Stack Component
-const OnboardingStack = () => {
-  return (
-    <Stack.Navigator
-      screenOptions={{
-        headerShown: false,
-      }}>
-      <Stack.Screen name="Welcome" component={WelcomeScreen} />
-      {/* <Stack.Screen name="Login" component={LoginScreen} /> */}
-      {/* <Stack.Screen name="Signup" component={SignupScreen} /> */}
-    </Stack.Navigator>
-  );
-};
-
-// LoggedIn Stack Component
-
-const LoggedInStack = () => {
-  return (
-    <Stack.Navigator
-      screenOptions={{
-        // headerStyle: {
-        //   backgroundColor: '#f4511e',
-        // },
-        // headerTintColor: '#fff',
-        // headerTitleStyle: {
-        //   fontWeight: 'bold',
-        // },
-        headerShown: false,
-      }}>
-      <Stack.Screen
-        name="Home"
-        component={HomeScreen}
-        // options={{
-        //   title: 'Dashboard',
-        // }}
-      />
-      {/* <Stack.Screen
-        name="Profile"
-        component={ProfileScreen}
-        options={{
-          title: 'My Profile',
-        }}
-      />
-      <Stack.Screen
-        name="Settings"
-        component={SettingsScreen}
-        options={{
-          title: 'Settings',
-        }}
-      /> */}
-    </Stack.Navigator>
-  );
-};
-
-const TimerStack = () => {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={TimerHomeScreen} />
-      <Stack.Screen name="History" component={HistoryScreen} />
-    </Stack.Navigator>
-  );
-};
-
-export {OnboardingStack, LoggedInStack, TimerStack, StarWarsStack};
+export {StarWarsStack};
